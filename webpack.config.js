80d765320e2ef7ed69332e6d16967304d1181485
Steven Pelletier <steve.d.pelletier@gmail.com>
2024-07-02 13:18:32 -0400

    main push

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
-    assetModuleFilename: 'images/[hash][ext][query]',
+    filename: 'bundle.js',
+    assetModuleFilename: 'images/[name][ext][query]',
     publicPath: '/decision-engine-v2/',
     clean: true,
   },
@@ -36,7 +37,19 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'styles.[contenthash].css',
+      filename: 'styles.css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/data'),
+          to: path.resolve(__dirname, 'dist/data'),
+        },
+        {
+          from: path.resolve(__dirname, 'public/doctors_procedures.json'),
+          to: path.resolve(__dirname, 'dist/doctors_procedures.json'),
+        },
+      ],
     }),
   ],
   devServer: {
